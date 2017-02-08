b086fec1654a17445f1302cd1fd2abca00394c67
Brendan Bouffard <bouffard.brendan.alt@gmail.comgit config -lgit config -lgit config --global user.name Brendan>
2017-02-07 22:27:37 -0500

    added item resource and validations to text

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,67 @@
+class ItemsController < ApplicationController
+  
+
+  # GET /items
+  # GET /items.json
+  def index
+    @items = Item.all
+  end
+
+  # GET /items/1
+  # GET /items/1.json
+  def show
+    @item =Item.find(params[:id])
+  end
+
+  # GET /items/new
+  def new
+    @item = current_user.items.new
+  end
+
+  # GET /items/1/edit
+  def edit
+    @item =current_user.items.find(params[:id])
+  end
+
+  # POST /items
+  # POST /items.json
+  def create
+    @item = current_user.items.new(item_params)
+
+    if @item.save
+      redirect_to @item, notice: 'Item was successfully created'
+    else
+     render :new
+    end
+  end
+
+
+  # PATCH/PUT /items/1
+  # PATCH/PUT /items/1.json
+  def update
+    @item =current_user.items.find(params[:id])
+      if @item.update(item_params)
+        redirect_to @item, notice: 'Item was successfully updated.'
+      else
+       render :edit
+  end
+end
+
+  # DELETE /items/1
+  # DELETE /items/1.json
+  def destroy
+    @item =current_user.items.find(params[:id])
+    @item.destroy
+    redirect_to items_url, notice: 'Item was successfully destroyed.'
+    end
+
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def item_params
+      params.require(:item).permit(:title, :url, :text)
+    end
+end
