eb98b835e3f53b0f942288889263fc694fe96149
Brendan Bouffard <bouffard.brendan.alt@gmail.comgit config -lgit config -lgit config --global user.name Brendan>
2017-02-08 14:47:46 -0500

    added username and name

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -12,15 +12,24 @@ ActiveAdmin.register User do
 #   permitted << :other if params[:action] == 'create' && current_user.admin?
 #   permitted
 # end
-	permit_params :email, :admin
+	permit_params :email, :admin, :name, :username
 
 	form do |f|
 		f.semantic_errors
 		f.inputs do
+			f.input :name
+			f.input :username
 			f.input :email
 			f.input :admin
 		end
 		f.actions
+	end
+	index do
+		id_column
+		column :name
+		column :username
+		column :email
+		column :admin
 	end
 
 end
